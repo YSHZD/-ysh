d433f275023157b1c1fdd5741c3ffcf984c5e262
YSH <1542228773@qq.com>
2018-04-02 17:04:00 +0800

    add some

--- xz/promise.js
+++ xz/promise.js
@@ -882,4 +882,478 @@ return thunk() * 2;
 上面代码中，函数 f 的参数 x + 5  被一个函数替换了。凡是用到原参数的地方，对 Thunk  函数求值即可。
 这就是 Thunk 函数的定义，它是 " 传名调用 " 的一种实现策略，用来替换某个表达式。
 
+JavaScript 语言的 Thunk 函数
+JavaScript 语言是传值调用，它的 Thunk 函数含义有所不同。在 JavaScript 语言中， Thunk 函数替换的不是表达
+式，而是多参数函数，将其替换成单参数的版本，且只接受回调函数作为参数。
+//  正常版本的 readFile （多参数版本）
+fs.readFile(fileName, callback);
+// Thunk 版本的 readFile （单参数版本）
+var readFileThunk = Thunk(fileName);
+readFileThunk(callback);
+var Thunk = function (fileName){
+return function (callback){
+return fs.readFile(fileName, callback);
+};
+};
+上面代码中， fs 模块的 readFile 方法是一个多参数函数，两个参数分别为文件名和回调函数。经过转换器处
+理，它变成了一个单参数函数，只接受回调函数作为参数。这个单参数版本，就叫做 Thunk 函数。
+任何函数，只要参数有回调函数，就能写成 Thunk 函数的形式。下面是一个简单的 Thunk 函数转换器。
+
+var Thunk = function(fn){
+return function (){
+var args = Array.prototype.slice.call(arguments);
+return function (callback){
+args.push(callback);
+return fn.apply(this, args);
+}
+};
+};
+使用上面的转换器，生成 fs.readFile  的 Thunk 函数。
+var readFileThunk = Thunk(fs.readFile);
+readFileThunk(fileA)(callback);
+
+Thunkify 模块
+生产环境的转换器，建议使用 Thunkify 模块。
+首先是安装。
+$ npm install thunkify
+使用方式如下。
+var thunkify = require('thunkify');
+var fs = require('fs');
+var read = thunkify(fs.readFile);
+read('package.json')(function(err, str){
+// ...
+});
+Thunkify 的源码与上一节那个简单的转换器非常像。
+
+Generator  函数的流程管理
+你可能会问， Thunk 函数有什么用？回答是以前确实没什么用，但是 ES6 有了 Generator 函数， Thunk 函数现在
+可以用于 Generator 函数的自动流程管理。
+以读取文件为例。下面的 Generator 函数封装了两个异步操作。
+var fs = require('fs');
+var thunkify = require('thunkify');
+var readFile = thunkify(fs.readFile);
+var gen = function* (){
+var r1 = yield readFile('/etc/fstab');
+console.log(r1.toString());
+var r2 = yield readFile('/etc/shells');
+console.log(r2.toString());
+};
+上面代码中， yield 命令用于将程序的执行权移出 Generator 函数，那么就需要一种方法，将执行权再交还给
+Generator 函数。
+这种方法就是 Thunk 函数，因为它可以在回调函数里，将执行权交还给 Generator 函数。为了便于理解，我们
+先看如何手动执行上面这个 Generator 函数。
+var g = gen();
+var r1 = g.next();
+r1.value(function(err, data){
+if (err) throw err;
+var r2 = g.next(data);
+r2.value(function(err, data){
+if (err) throw err;
+g.next(data);
+});
+});
+上面代码中，变量 g 是 Generator 函数的内部指针，表示目前执行到哪一步。 next 方法负责将指针移动到下一
+步，并返回该步的信息（ value 属性和 done 属性）。
+仔细查看上面的代码，可以发现 Generator 函数的执行过程，其实是将同一个回调函数，反复传入 next 方法的
+value 属性。这使得我们可以用递归来自动完成这个过程。
+
+Thunk 函数的自动流程管理
+Thunk 函数真正的威力，在于可以自动执行 Generator 函数。下面就是一个基于 Thunk 函数的 Generator 执行器。
+function run(fn) {
+var gen = fn();
+function next(err, data) {
+var result = gen.next(data);
+if (result.done) return;
+result.value(next);
+}
+next();
+}
+run(gen);
+上面代码的 run 函数，就是一个 Generator 函数的自动执行器。内部的 next 函数就是 Thunk 的回调函数。 next 函数
+先将指针移到 Generator 函数的下一步（ gen.next 方法），然后判断 Generator 函数是否结束（ result.done  属
+性），如果没结束，就将 next 函数再传入 Thunk 函数（ result.value 属性），否则就直接退出。
+有了这个执行器，执行 Generator 函数方便多了。不管有多少个异步操作，直接传入 run  函数即可。当然，
+前提是每一个异步操作，都要是 Thunk 函数，也就是说，跟在 yield  命令后面的必须是 Thunk 函数。
+var gen = function* (){
+var f1 = yield readFile('fileA');
+var f2 = yield readFile('fileB');
+// ...
+var fn = yield readFile('fileN');
+};
+run(gen);
+上面代码中，函数 gen  封装了 n  个异步的读取文件操作，只要执行 run  函数，这些操作就会自动完
+成。这样一来，异步操作不仅可以写得像同步操作，而且一行代码就可以执行。
+Thunk 函数并不是 Generator 函数自动执行的唯一方案。因为自动执行的关键是，必须有一种机制，自动控制
+Generator 函数的流程，接收和交还程序的执行权。回调函数可以做到这一点， Promise  对象也可以做到这一
+点。
+
+co 模块
+基本用法
+co 模块是著名程序员 TJ Holowaychuk 于 2013 年 6 月发布的一个小工具，用于 Generator 函数的自动执行。
+比如，有一个 Generator 函数，用于依次读取两个文件。
+var gen = function* (){
+var f1 = yield readFile('/etc/fstab');
+var f2 = yield readFile('/etc/shells');
+console.log(f1.toString());
+console.log(f2.toString());
+};
+co 模块可以让你不用编写 Generator 函数的执行器。
+var co = require('co');
+co(gen);
+上面代码中， Generator 函数只要传入 co 函数，就会自动执行。
+co 函数返回一个 Promise 对象，因此可以用 then 方法添加回调函数。
+co(gen).then(function (){
+console.log('Generator  函数执行完成 ');
+})
+上面代码中，等到 Generator 函数执行结束，就会输出一行提示。
+
+co 模块的原理
+为什么 co 可以自动执行 Generator 函数？
+前面说过， Generator 就是一个异步操作的容器。它的自动执行需要一种机制，当异步操作有了结果，能够自
+动交回执行权。
+两种方法可以做到这一点。
+（ 1 ）回调函数。将异步操作包装成 Thunk 函数，在回调函数里面交回执行权。
+（ 2 ） Promise  对象。将异步操作包装成 Promise 对象，用 then 方法交回执行权。
+co 模块其实就是将两种自动执行器（ Thunk 函数和 Promise 对象），包装成一个模块。使用 co 的前提条件
+是， Generator 函数的 yield 命令后面，只能是 Thunk 函数或 Promise 对象。
+上一节已经介绍了基于 Thunk 函数的自动执行器。下面来看，基于 Promise 对象的自动执行器。这是理解 co 模
+块必须的。
+
+基于 Promise 对象的自动执行
+还是沿用上面的例子。首先，把 fs 模块的 readFile 方法包装成一个 Promise 对象。
+var fs = require('fs');
+var readFile = function (fileName){
+return new Promise(function (resolve, reject){
+fs.readFile(fileName, function(error, data){
+if (error) reject(error);
+resolve(data);
+});
+});
+};
+var gen = function* (){
+var f1 = yield readFile('/etc/fstab');
+var f2 = yield readFile('/etc/shells');
+console.log(f1.toString());
+console.log(f2.toString());
+};
+然后，手动执行上面的 Generator 函数。
+var g = gen();
+g.next().value.then(function(data){
+g.next(data).value.then(function(data){
+g.next(data);
+});
+})
+手动执行其实就是用 then 方法，层层添加回调函数。理解了这一点，就可以写出一个自动执行器。
+function run(gen){
+var g = gen();
+function next(data){
+var result = g.next(data);
+if (result.done) return result.value;
+result.value.then(function(data){
+next(data);
+});
+}
+next();
+}
+run(gen);
+上面代码中，只要 Generator 函数还没执行到最后一步， next 函数就调用自身，以此实现自动执行。
+
+co 模块的源码
+co 就是上面那个自动执行器的扩展，它的源码只有几十行，非常简单。
+首先， co 函数接受 Generator 函数作为参数，返回一个 Promise  对象。
+function co(gen) {
+var ctx = this;
+return new Promise(function(resolve, reject) {
+});
+}
+在返回的 Promise 对象里面， co 先检查参数 gen 是否为 Generator 函数。如果是，就执行该函数，得到一个内部
+指针对象；如果不是就返回，并将 Promise 对象的状态改为 resolved 。
+function co(gen) {
+var ctx = this;
+return new Promise(function(resolve, reject) {
+if (typeof gen === 'function') gen = gen.call(ctx);
+if (!gen || typeof gen.next !== 'function') return resolve(gen);
+});
+}
+接着， co 将 Generator 函数的内部指针对象的 next 方法，包装成 onFulfilled 函数。这主要是为了能够捕捉抛出的
+错误。
+
+
+含义
+ES7 提供了 async  函数，使得异步操作变得更加方便。 async  函数是什么？一句话， async  函数就是
+Generator 函数的语法糖。
+前文有一个 Generator 函数，依次读取两个文件。
+var fs = require('fs');
+var readFile = function (fileName){
+return new Promise(function (resolve, reject){
+fs.readFile(fileName, function(error, data){
+if (error) reject(error);
+resolve(data);
+});
+});
+};
+var gen = function* (){
+var f1 = yield readFile('/etc/fstab');
+var f2 = yield readFile('/etc/shells');
+console.log(f1.toString());
+console.log(f2.toString());
+};
+写成 async  函数，就是下面这样。
+var asyncReadFile = async function (){
+var f1 = await readFile('/etc/fstab');
+var f2 = await readFile('/etc/shells');
+console.log(f1.toString());
+console.log(f2.toString());
+};
+一比较就会发现， async  函数就是将 Generator 函数的星号（ *  ）替换成 async  ，将 yield  替换
+成 await  ，仅此而已。
+
+async  函数对 Generator  函数的改进，体现在以下四点。
+（ 1 ）内置执行器。 Generator 函数的执行必须靠执行器，所以才有了 co  模块，而 async  函数自带执行
+器。也就是说， async  函数的执行，与普通函数一模一样，只要一行。
+var result = asyncReadFile();
+上面的代码调用了 asyncReadFile  函数，然后它就会自动执行，输出最后结果。这完全不像 Generator 函
+数，需要调用 next  方法，或者用 co  模块，才能得到真正执行，得到最后结果
+
+（ 2 ）更好的语义。 async  和 await  ，比起星号和 yield  ，语义更清楚了。 async  表示函数里有异步
+操作， await  表示紧跟在后面的表达式需要等待结果。
+（ 3 ）更广的适用性。 co  模块约定， yield  命令后面只能是 Thunk 函数或 Promise 对象，而 async  函数
+的 await  命令后面，可以是 Promise 对象和原始类型的值（数值、字符串和布尔值，但这时等同于同步操
+作）。
+（ 4 ）返回值是 Promise 。 async  函数的返回值是 Promise 对象，这比 Generator 函数的返回值是 Iterator 对象方
+便多了。你可以用 then  方法指定下一步的操作。
+进一步说， async  函数完全可以看作多个异步操作，包装成的一个 Promise 对象，而 await  命令就是内
+部 then  命令的语法糖。
+正常情况下， await  命令后面是一个 Promise 对象，否则会被转成 Promise 。
+下面是一个完整的例子。
+async function getTitle(url) {
+let response = await fetch(url);
+let html = await response.text();
+return html.match(/<title>([\s\S]+)<\/title>/i)[1];
+}
+getTitle('https://tc39.github.io/ecma262/').then(console.log)
+// "ECMAScript 2017 Language Specification"
+async 函数的实现
+async  函数的实现，就是将 Generator  函数和自动执行器，包装在一个函数里。
+async function fn(args){
+// ...
+}
+//  等同于
+function fn(args){
+return spawn(function*() {
+// ...
+});
+}
+所有的 async  函数都可以写成上面的第二种形式，其中的 spawn  函数就是自动执行器。
+下面给出 spawn  函数的实现，基本就是前文自动执行器的翻版。
+function spawn(genF) {
+return new Promise(function(resolve, reject) {
+var gen = genF();
+function step(nextF) {
+try {
+var next = nextF();
+} catch(e) {
+return reject(e);
+}
+if(next.done) {
+return resolve(next.value);
+}
+Promise.resolve(next.value).then(function(v) {
+step(function() { return gen.next(v); });
+}, function(e) {
+step(function() { return gen.throw(e); });
+});
+}
+step(function() { return gen.next(undefined); });
+});
+}
+async  函数是非常新的语法功能，新到都不属于 ES6 ，而是属于 ES7 。目前，它仍处于提案阶段，但是转
+码器 Babel  和 regenerator  都已经支持，转码后就能使用
+
+
+async  函数的用法
+同 Generator 函数一样， async  函数返回一个 Promise 对象，可以使用 then  方法添加回调函数。当函数执
+行的时候，一旦遇到 await  就会先返回，等到触发的异步操作完成，再接着执行函数体内后面的语句。
+下面是一个例子。
+async function getStockPriceByName(name) {
+var symbol = await getStockSymbol(name);
+var stockPrice = await getStockPrice(symbol);
+return stockPrice;
+}
+getStockPriceByName('goog').then(function (result) {
+console.log(result);
+});
+上面代码是一个获取股票报价的函数，函数前面的 async  关键字，表明该函数内部有异步操作。调用该函
+数时，会立即返回一个 Promise  对象。
+下面的例子，指定多少毫秒后输出一个值。
+function timeout(ms) {
+return new Promise((resolve) => {
+setTimeout(resolve, ms);
+});
+}
+async function asyncPrint(value, ms) {
+await timeout(ms);
+console.log(value)
+}
+asyncPrint('hello world', 50);
+上面代码指定 50 毫秒以后，输出 "hello world" 。
+Async 函数有多种使用形式。
+//  函数声明
+async function foo() {}
+//  函数表达式
+const foo = async function () {};
+//  对象的方法
+let obj = { async foo() {} }
+//  箭头函数
+const foo = async () => {};
+注意点
+第一点， await  命令后面的 Promise 对象，运行结果可能是 rejected ，所以最好把 await  命令放
+在 try...catch  代码块中。
+async function myFunction() {
+try {
+await somethingThatReturnsAPromise();
+} catch (err) {
+console.log(err);
+}
+}
+//  另一种写法
+async function myFunction() {
+await somethingThatReturnsAPromise().catch(function (err){
+console.log(err);
+};
+}
+第二点，多个 await  命令后面的异步操作，如果不存在继发关系，最好让它们同时触发。
+let foo = await getFoo();
+let bar = await getBar();
+上面代码中， getFoo  和 getBar  是两个独立的异步操作（即互不依赖），被写成继发关系。这样比较耗
+时，因为只有 getFoo  完成以后，才会执行 getBar  ，完全可以让它们同时触发。
+//  写法一
+let [foo, bar] = await Promise.all([getFoo(), getBar()]);
+//  写法二
+let fooPromise = getFoo();
+let barPromise = getBar();
+let foo = await fooPromise;
+let bar = await barPromise;
+上面两种写法， getFoo  和 getBar  都是同时触发，这样就会缩短程序的执行时间。
+第三点， await  命令只能用在 async  函数之中，如果用在普通函数，就会报错。
+async function dbFuc(db) {
+let docs = [{}, {}, {}];
+//  报错
+docs.forEach(function (doc) {
+await db.post(doc);
+});
+}
+上面代码会报错，因为 await 用在普通函数之中了。但是，如果将 forEach  方法的参数改成 async  函数，
+也有问题。
+async function dbFuc(db) {
+let docs = [{}, {}, {}];
+//  可能得到错误结果
+docs.forEach(async function (doc) {
+await db.post(doc);
+});
+}
+上面代码可能不会正常工作，原因是这时三个 db.post  操作将是并发执行，也就是同时执行，而不是继发
+执行。正确的写法是采用 for  循环。
+async function dbFuc(db) {
+let docs = [{}, {}, {}];
+for (let doc of docs) {
+await db.post(doc);
+}
+}
+如果确实希望多个请求并发执行，可以使用 Promise.all  方法。
+async function dbFuc(db) {
+let docs = [{}, {}, {}];
+let promises = docs.map((doc) => db.post(doc));
+let results = await Promise.all(promises);
+console.log(results);
+}
+//  或者使用下面的写法
+async function dbFuc(db) {
+let docs = [{}, {}, {}];
+let promises = docs.map((doc) => db.post(doc));
+let results = [];
+for (let promise of promises) {
+results.push(await promise);
+}
+console.log(results);
+}
+ES6 将 await  增加为保留字。使用这个词作为标识符，在 ES5 是合法的，在 ES6 将抛出 SyntaxError 。
+
+
+
+与 Promise 、 Generator 的比较
+我们通过一个例子，来看 Async 函数与 Promise 、 Generator 函数的区别。
+
+首先是 Promise 的写法。
+function chainAnimationsPromise(elem, animations) {
+//  变量 ret 用来保存上一个动画的返回值
+var ret = null;
+//  新建一个空的 Promise
+var p = Promise.resolve();
+//  使用 then 方法，添加所有动画
+for(var anim in animations) {
+p = p.then(function(val) {
+ret = val;
+return anim(elem);
+})
+}
+//  返回一个部署了错误捕捉机制的 Promise
+return p.catch(function(e) {
+/*  忽略错误，继续执行 */
+}).then(function() {
+return ret;
+});
+}
+虽然 Promise 的写法比回调函数的写法大大改进，但是一眼看上去，代码完全都是 Promise 的 API （ then 、 catch
+等等），操作本身的语义反而不容易看出来。
+
+
+接着是 Generator 函数的写法。
+function chainAnimationsGenerator(elem, animations) {
+return spawn(function*() {
+var ret = null;
+try {
+for(var anim of animations) {
+ret = yield anim(elem);
+}
+} catch(e) {
+/*  忽略错误，继续执行 */
+}
+return ret;
+});
+}
+上面代码使用 Generator 函数遍历了每个动画，语义比 Promise 写法更清晰，用户定义的操作全部都出现在
+spawn 函数的内部。这个写法的问题在于，必须有一个任务运行器，自动执行 Generator 函数，上面代码的
+spawn 函数就是自动执行器，它返回一个 Promise 对象，而且必须保证 yield 语句后面的表达式，必须返回一个
+Promise 。
+最后是 Async 函数的写法。
+
+接着是 Generator 函数的写法。
+function chainAnimationsGenerator(elem, animations) {
+return spawn(function*() {
+var ret = null;
+try {
+for(var anim of animations) {
+ret = yield anim(elem);
+}
+} catch(e) {
+/*  忽略错误，继续执行 */
+}
+return ret;
+});
+}
+上面代码使用 Generator 函数遍历了每个动画，语义比 Promise 写法更清晰，用户定义的操作全部都出现在
+spawn 函数的内部。这个写法的问题在于，必须有一个任务运行器，自动执行 Generator 函数，上面代码的
+spawn 函数就是自动执行器，它返回一个 Promise 对象，而且必须保证 yield 语句后面的表达式，必须返回一个
+Promise 。
+最后是 Async 函数的写法。
+
+
+
+
+
+
+
+
+
 
